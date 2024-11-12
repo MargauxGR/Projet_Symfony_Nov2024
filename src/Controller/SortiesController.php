c23cc9e1e2fd571caf5f38a4ffb9a938b4b71643
mgarnier2024 <margaux.garnier2024@campus-eni.fr>
2024-11-12 16:54:24 +0100

    Commit formulaire partie 1

--- src/Controller/SortiesController.php
+++ src/Controller/SortiesController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -15,6 +19,39 @@ class SortiesController extends AbstractController
         $sorties = $sortieRepository->findAll();
         return $this->render('sorties/indsorties.html.twig', [
             'sorties' => $sorties,
+        ]);
+    }
+
+    //modifier le nom de le route et son url, le nom de méthode aussi
+    #[Route('/sorties', name: 'app_sorties_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        /////////////////////////////////////////////
+        //configurer le formulaire, dans une autre class qui termine _____type
+        //utiliser une commnande pour ça
+        /////////////////////////////////////
+
+
+        //créer une instance du formulaire et l'associer avec une instance de sortie vide
+        //récupérer le requête HTTP pour récupérer les données du formulaire
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+        $sortieForm->handleRequest($request);
+
+        //tester si le formulaire est soumis et si'il est valide
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            //enregister la donnée
+            $em->persist($sortie);
+            $em->flush();
+            $this->addFlash('success', 'Votre sortie a été ajoutée');
+            return $this->redirectToRoute('app_sorties');
+        }
+
+        //rediriger sur la page d'accueil
+
+        //sinon renvoyer un nouveau template avec le code du formulaire
+        return $this->render('sorties_form/sortiesform.html.twig', [
+            ["sortie" => $sortie],
         ]);
     }
 }
